472c58d64820abfaaf19dc77c405df214683e0e8
Carter Struck <carterstruck02@gmail.com>
2024-10-27 16:53:48 -0400

    working on adding items to list

--- Assignment5.lisp
+++ Assignment5.lisp
@@ -4,3 +4,20 @@
 ;(4 (7 22) "art" ("math" (8) 99) 100)
 
 (myList)
+
+(defun leapYear (&optional (currentYearNum 1800) (leapYearList '()))
+
+    (when (<= currentYearNum 2024) ; base case check
+        (if (= (mod currentYearNum 4) 0) ; checkign for year to mod 0
+            (if (mod currentYearNum 100) 
+                ;(format t "~A is a leap year~%" currentYearNum) 
+                (car  ; first condition of not 100 
+                (if (not (zerop(mod currentyearnum 400))) 
+                    (push currentyearnum leapYearList)) ; then if mod 400 
+            ); when
+        ) ; if mod
+        (leapYear (+ currentyearnum 1) (cdr leapYearList))  ; calls again within when < 2024)
+    leapYearList) ; when / return the list
+); leapYear
+;(leapYear)
+
